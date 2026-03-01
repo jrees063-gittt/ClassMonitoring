75b8f594e7293a249701121b3ca2f049ea2412f1
Mufeedha Maheen <mufeedhamaheen999@gmail.com>
2026-03-01 23:08:54 +0530

    Changed Backend login essentials

--- monitoring/views.py
+++ monitoring/views.py
@@ -10,24 +10,21 @@ import json
 @csrf_exempt
 def login_view(request):
 
-    if request.method == "POST":
+    if request.method != "POST":
+        return JsonResponse({"error": "POST required"}, status=405)
+
+    try:
         data = json.loads(request.body)
+    except:
+        return JsonResponse({"error": "Invalid JSON"}, status=400)
 
-        username = data.get("username")
-        password = data.get("password")
+    username = data.get("username")
+    password = data.get("password")
 
-        user = authenticate(request, username=username, password=password)
+    user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)   # creates session
-            return JsonResponse({
-                "status": "success",
-                "message": "Login successful"
-            })
-        else:
-            return JsonResponse({
-                "status": "error",
-                "message": "Invalid credentials"
-            }, status=401)
-
-    return JsonResponse({"error": "Invalid request"}, status=400)
+    if user:
+        login(request, user)
+        return JsonResponse({"status": "success"})
+    else:
+        return JsonResponse({"error": "Invalid credentials"}, status=401)
